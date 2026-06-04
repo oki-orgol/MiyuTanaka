e017f522cdc8734df0f9cda472407821aa28f1b6
MiyuTanaka <kaisinnoitigeki999@outlook.jp>
2026-06-04 17:03:54 +0900

    コード入力

---
+++ 20260529/6/06task.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+// 1以上100以下の、7の倍数を出力
+
+int main(void)
+{
+    const int FIRST_NUMBER = 1;
+    const int LAST_NUMBER = 100;
+    const int CONDITION_NUMBER = 7;
+
+    for (int i = FIRST_NUMBER; i <= LAST_NUMBER; i++)
+    {
+        if (i % CONDITION_NUMBER == 0)
+        {
+            cout << i << endl;
+        }
+    }
+}
